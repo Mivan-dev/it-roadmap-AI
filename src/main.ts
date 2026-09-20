5be5226a092bd29a854a24f3933835b01d72103f
IvanM <sirius.ivo@gmail.com>
2026-09-20 03:06:54 -0300

    feat: update CORS policy to allow requests from specific origins

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,9 @@ import { AppModule } from './app.module.js';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  app.enableCors({
-    origin: '*',
-  });
+ app.enableCors({
+  origin: ['http://localhost:4200', 'https://it-roadmap-ai.netlify.app'],
+});
 
   await app.listen(process.env.PORT ?? 3000);
 }
